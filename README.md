50395aeb0960270719a44dc7fb48ceed715121aa
nam <you@example.com>
2023-04-18 15:02:42 +0700

    Responsive Header

---
+++ README.md
@@ -0,0 +1,9 @@
+## Tailwindcss Design UI Coffee Style. 17/04/2023.
+
+Clone project and run test.
+
+...
+$ git clone https://git_url_clone 
+$ cd <project_name>
+$ npm run build:css
+$ npm run start:dev
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,23 @@ module.exports = {
     content: ['./public/**/*.{html,js}'],
     mode: 'jit',
     theme: {
-        extend: { 
-            // fontFamily: {
-            //     'karla': ['Karla', sans - serif],
-            // },
+        extend: {
+            fontFamily: {
+                Karla: ['Karla', 'sans - serif'],
+            },
             colors: {
-                'primary--color': '#BF3325'
+                'primary--color': '#BF3325',
+            },
+            keyframes: {
+                slideDown: {
+                    '0%': { transform: 'translateY(-100%)' },
+                    '100%': { transform: 'translateY(0)' },
+                },
+            },
+            animation: {
+                'slideDown': 'slideDown 0.5s ease-in-out',
             }
         },
-        
     },
     plugins: [],
 };
